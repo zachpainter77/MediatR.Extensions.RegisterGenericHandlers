3ed6c9a962a7aa0930402508f9f49becc5b09aef
zach painter <zachpainter77@gmail.com>
2024-03-25 14:53:51 -0700

    Add project files.

---
+++ MediatR.Extensions.RegisterGenericHandlers/RegisterGenericHandlers.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MediatR.Extensions.RegisterGenericHandlers
+{
+    public static class RegisterGenericHandlers
+    {
+        public static IServiceCollection RegisterGenericMediatorHandlers(this IServiceCollection services, IEnumerable<Assembly> assembliesToScan, Func<Type, bool>? typeEvaluator = null)
+        {
+            typeEvaluator ??= t => true;
+
+            var openRequestHandlerInterfaces = new Type[] {
+                    typeof(IRequestHandler<>),
+                    typeof(IRequestHandler<,>)
+            };
+
+            foreach (var openRequestHandlerInterface in openRequestHandlerInterfaces)
+            {
+
+                var genericHandlers = assembliesToScan
+                    .SelectMany(assembly => assembly.DefinedTypes)
+                    .Where(type => type.IsConcrete() && type.IsOpenGeneric() && type.FindInterfacesThatClose(openRequestHandlerInterface).Any())
+                    .Where(typeEvaluator)
+                    .ToList();
+
+                AddAllConcretionsThatClose(openRequestHandlerInterface, genericHandlers, services, assembliesToScan);
+            }
+
+            return services;
+        }
+
+        private static (Type Service, Type Implementation) GetConcreteRegistrationTypes(Type openRequestHandlerInterface, Type concreteGenericTRequest, Type openRequestHandlerImplementation)
+        {
+            var closingType = concreteGenericTRequest.GetGenericArguments().First();
+
+            var concreteTResponse = concreteGenericTRequest.GetInterfaces()
+                .FirstOrDefault(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IRequest<>))
+                ?.GetGenericArguments()
+                .FirstOrDefault();
+
+            var typeDefinition = openRequestHandlerInterface.GetGenericTypeDefinition();
+
+            var serviceType = concreteTResponse != null ?
+                typeDefinition.MakeGenericType(concreteGenericTRequest, concreteTResponse) :
+                typeDefinition.MakeGenericType(concreteGenericTRequest);
+
+            return (serviceType, openRequestHandlerImplementation.MakeGenericType(closingType));
+        }
+
+        private static List<Type>? GetConcreteRequestTypes(Type openRequestHandlerInterface, Type openRequestHandlerImplementation, IEnumerable<Assembly> assembliesToScan)
+        {
+            var constraints = openRequestHandlerImplementation.GetGenericArguments().First().GetGenericParameterConstraints();
+
+            var typesThatCanClose = assembliesToScan
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => type.IsClass && !type.IsAbstract && constraints.All(constraint => constraint.IsAssignableFrom(type)))
+                .ToList();
+
+            var requestType = openRequestHandlerInterface.GenericTypeArguments.First();
+
+            if (requestType.IsGenericParameter)
+                return null;
+
+            var requestGenericTypeDefinition = requestType.GetGenericTypeDefinition();
+
+            return typesThatCanClose.Select(type => requestGenericTypeDefinition.MakeGenericType(type)).ToList();
+        }
+
+        private static void AddAllConcretionsThatClose(Type openRequestInterface, List<Type> concretions, IServiceCollection services, IEnumerable<Assembly> assembliesToScan)
+        {
+            foreach (var concretion in concretions)
+            {
+                var concreteRequests = GetConcreteRequestTypes(openRequestInterface, concretion, assembliesToScan);
+
+                if (concreteRequests is null)
+                    continue;
+
+                var registrationTypes = concreteRequests
+                    .Select(concreteRequest => GetConcreteRegistrationTypes(openRequestInterface, concreteRequest, concretion));
+
+                foreach (var (Service, Implementation) in registrationTypes)
+                {
+                    services.AddTransient(Service, Implementation);
+                }
+            }
+        }
+
+        internal static bool CouldCloseTo(this Type openConcretion, Type closedInterface)
+        {
+            var openInterface = closedInterface.GetGenericTypeDefinition();
+            var arguments = closedInterface.GenericTypeArguments;
+
+            var concreteArguments = openConcretion.GenericTypeArguments;
+            return arguments.Length == concreteArguments.Length && openConcretion.CanBeCastTo(openInterface);
+        }
+
+        private static bool CanBeCastTo(this Type pluggedType, Type pluginType)
+        {
+            if (pluggedType == null) return false;
+
+            if (pluggedType == pluginType) return true;
+
+            return pluginType.IsAssignableFrom(pluggedType);
+        }
+
+        private static bool IsOpenGeneric(this Type type)
+        {
+            return type.IsGenericTypeDefinition || type.ContainsGenericParameters;
+        }
+
+        internal static IEnumerable<Type> FindInterfacesThatClose(this Type pluggedType, Type templateType)
+        {
+            return FindInterfacesThatClosesCore(pluggedType, templateType).Distinct();
+        }
+
+        private static IEnumerable<Type> FindInterfacesThatClosesCore(Type pluggedType, Type templateType)
+        {
+            if (pluggedType == null) yield break;
+
+            if (!pluggedType.IsConcrete()) yield break;
+
+            if (templateType.IsInterface)
+            {
+                foreach (
+                    var interfaceType in
+                    pluggedType.GetInterfaces()
+                        .Where(type => type.IsGenericType && (type.GetGenericTypeDefinition() == templateType)))
+                {
+                    yield return interfaceType;
+                }
+            }
+            else if (pluggedType.BaseType!.IsGenericType &&
+                     (pluggedType.BaseType!.GetGenericTypeDefinition() == templateType))
+            {
+                yield return pluggedType.BaseType!;
+            }
+
+            if (pluggedType.BaseType == typeof(object)) yield break;
+
+            foreach (var interfaceType in FindInterfacesThatClosesCore(pluggedType.BaseType!, templateType))
+            {
+                yield return interfaceType;
+            }
+        }
+
+        private static bool IsConcrete(this Type type)
+        {
+            return !type.IsAbstract && !type.IsInterface;
+        }
+    }
+}
